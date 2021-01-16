c3f06e2ea8658daa9c0b2bb6d9f66bb4f1714d94
Hung.VS <HungICT2407@gmail.com>
2021-01-17 00:54:06 +0700

    Set up Java & requirements

--- scripts/elephas-environment-setup.sh
+++ scripts/elephas-environment-setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Check the status:
+if [ "$ELEPHAS_ENVIRONMENT_SETUP_STATUS" == "1" ]; then
+    exit
+fi
+
 # [NOTES]: Environment setup for Elephas 1.0.0
 # 1. Run with Bash Shell.
 # 2. Use "chmod +x elephas-environment-setup.sh" to be able to run this script.
+# 3. Use "sudo rm -rf .pyenv" to remove the ".pyenv" directory.
 
 # [STEPS]: 
 
@@ -20,8 +26,10 @@ echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 # 3. Add "pyenv init" to your shell to enable shims and autocompletion:
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
 
-# 4. Restart the shell:
-exec "$SHELL"
+# 4. Load the latest .bashrc:
+# * "exec "$SHELL"": Open new shell process.
+# * "." = "source".
+. ~/.bashrc
 
 
 # II. Install Python 3.7.9:
@@ -37,4 +45,32 @@ pyenv install 3.7.9
 
 # 3. Switch to Python 3.7.9:
 pyenv global 3.7.9
+
+
+# III. Install Java:
+# * Elephas needs Java to work.
+
+sudo apt-get update 
+sudo apt-get install default-jre
+export 'JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"' >> ~/.bashrc
+. ~/.bashrc
+
+# IV. Create a Python 3.7.9 virtual environment:
+
+# 1. Create & activate Python 3.7.9 virtual environment:
+
+# python -m venv venv
+# . venv/bin/activate
+
+# 2. Install all dependencies to run Elephas script:
+# pip install -r requirements.txt
+
+# V. Update Status: 
+# * "unset ELEPHAS_ENVIRONMENT_SETUP_STATUS": Remove the specified environment variable. 
+
+export ELEPHAS_ENVIRONMENT_SETUP_STATUS=1
+
+# [TODO]: 
+# 1. IV.
+# 2. "export".
 
